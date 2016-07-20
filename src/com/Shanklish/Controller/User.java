5eadd09708e930f1a81df3984621f6ffdf07a930
Tyler Hensley <Tyler Hensley>
2016-07-20 11:01:54 -0400

    Added login jsp and created a User class

---
+++ src/com/Shanklish/Controller/User.java
@@ -0,0 +1,34 @@
+package com.Shanklish.Controller;
+
+public class User {
+
+	private String username;
+	private String password;
+	private int id;
+
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
